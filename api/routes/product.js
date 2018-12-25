9e908f069020a858d98ed41a1003a1837f1ee8d9
demir6693 <demir_agovic@live.com>
2018-12-25 14:16:16 +0100

    get prod

--- api/routes/product.js
+++ api/routes/product.js
@@ -17,17 +17,29 @@ db.connect((err => {
     console.log('MySql Connected...');
 }));
 
-router.get('/', (req, res, next) => {
+router.get('/nesto', (req, res, next) => {
     res.status(200).json({
         message: 'get'
     });
 });
 
-router.get('/:chproudId', (req, res, next) => {
+router.get('/getprod/:chprodId', (req, res, next) => {
     
-    const id_podgrupe = req.params.chproudId;
+    const id_podgrupe = req.params.chprodId;
     
     let sql = `SELECT * FROM proizvodi WHERE id_podgrupe = ${id_podgrupe}`;
+            let query = db.query(sql, (err, result) => {
+            if(err) throw err;
+            res.json(result);
+            });
+});
+
+
+router.get('/getchgropu/:name', (req, res, next) => {
+    
+    const name_chg = req.params.name;
+
+    let sql = "SELECT * FROM podgrupe WHERE ime = '" + name_chg + "'";
             let query = db.query(sql, (err, result) => {
             if(err) throw err;
             res.json(result);
--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 
 const productRoutes = require('./api/routes/product');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        next();
+});
+
 app.use('/product', productRoutes);
 
 module.exports = app;
